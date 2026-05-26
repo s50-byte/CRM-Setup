4ca72481a1a845d02bf8a3111065fdae342b979b
Simon <smeier50@gmail.com>
2026-05-26 17:00:11 +0000

    Stand vor reset-testdaten.js

---
+++ backend/update-programme-namen.sql
@@ -0,0 +1,7 @@
+UPDATE programm SET name = 'Erstmalige berufliche Ausbildung' WHERE name = 'Ausbildung';
+UPDATE programm SET name = 'Beratung & Coaching'              WHERE name = 'Beratung';
+UPDATE programm SET name = 'Erstmalige berufliche Abklärung'  WHERE name = 'Abklärung';
+UPDATE programm SET name = 'Gezielte Vorbereitung'            WHERE name = 'Gez. Vorbereitung';
+UPDATE programm SET name = 'LE'                               WHERE name = 'Lernender';
+UPDATE programm SET name = 'TN'                               WHERE name = 'Teilnehmer';
+UPDATE programm SET name = 'MA'                               WHERE name = 'Mitarbeiter mit IV-Rente';
