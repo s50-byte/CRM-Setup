806f7c60a213799d58f8c6d1f53a817ee2a687a9
Simon <smeier50@gmail.com>
2026-06-15 14:46:09 +0000

    Migration: ausbildung_beruf/fachrichtung/lehrjahr Spaltenbreite erhöhen

---
+++ backend/add-klient-stammdaten-fix.sql
@@ -0,0 +1,3 @@
+ALTER TABLE dossier ALTER COLUMN ausbildung_beruf TYPE VARCHAR(100);
+ALTER TABLE dossier ALTER COLUMN ausbildung_fachrichtung TYPE VARCHAR(100);
+ALTER TABLE dossier ALTER COLUMN ausbildung_lehrjahr TYPE VARCHAR(20);
